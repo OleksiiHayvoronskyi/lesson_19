7dc0ba390f981d0710df9914ec37aa14f77f9c46
Oleksii Hayvoronskyi <oleksiihayvoronskyi@gmail.com>
2023-02-07 22:36:47 +0200

    Done

--- task_1.py
+++ task_1.py
@@ -9,25 +9,42 @@ print('--- Task 1 ---')
 
 G = nx.Graph()
 
-# Вузли.
-cities = [('Hadiach', 'Zinkiv', 45), ('Hadiach', 'Lebedyn', 60),
-          ('Hadiach', 'Myrhorod', 61), ('Hadiach', 'Zavodske', 67),
-          ('Lokhvytsia', 'Hadiach', 81), ('Hadiach', 'Lokhvytsia', 94)]
+# Визанчаю вузли та ребра (1-варіант).
+G.add_edge('Hadiach', 'Zinkiv', weight=45)
+G.add_edge('Hadiach', 'Lebedyn', weight=80)
+G.add_edge('Hadiach', 'Myrhorod', weight=200)
+G.add_edge('Hadiach', 'Zavodske', weight=100)
+G.add_edge('Lokhvytsia', 'Hadiach', weight=35)
+G.add_edge('Hadiach', 'Lokhvytsia', weight=40)
+G.add_edge('Lebedyn', 'Zavodske', weight=175)
+G.add_edge('Zinkiv', 'Lebedyn', weight=90)
+G.add_edge('Zinkiv', 'Zavodske', weight=150)
+G.add_edge('Lokhvytsia', 'Myrhorod', weight=100)
 
 
-# Визанчаю список ребер.
-edges = [('Hadiach', 'Zinkiv'), ('Zinkiv', 'Lebedyn'),
-         ('Lebedyn', 'Zavodske'), ('Hadiach', 'Zavodske'),
-         ('Myrhorod', 'Zavodske'), ('Lokhvytsia', 'Hadiach'),
-         ('Hadiach', 'Vorozhba')]
+# Визанчаю вузли (2-варіант).
+# cities = [('Hadiach', 'Zinkiv', 45), ('Hadiach', 'Lebedyn', 60),
+#           ('Hadiach', 'Myrhorod', 61), ('Hadiach', 'Zavodske', 67),
+#           ('Lokhvytsia', 'Hadiach', 81), ('Hadiach', 'Lokhvytsia', 94)]
+##
+# # Визанчаю список ребер.
+# edges = [('Hadiach', 'Zinkiv'), ('Zinkiv', 'Lebedyn'),
+#          ('Lebedyn', 'Zavodske'), ('Hadiach', 'Zavodske'),
+#          ('Myrhorod', 'Zavodske'), ('Lokhvytsia', 'Hadiach'),
+#          ('Hadiach', 'Vorozhba')]
 
 # Додаю інформацію в об’єкт графа.
-G.add_nodes_from(cities)
-G.add_edges_from(edges)
+# G.add_nodes_from(cities)
+# G.add_edges_from(edges)
 
+
+# Тип візуалізації circular_layout.
+pos=nx.circular_layout(G)
 # Візуалізую граф.
-nx.draw(G, with_labels=True, font_weight='light', font_size=7,
-        font_color='blue', node_color='y', node_shape='o', width=1.5,
+nx.draw(G, pos, with_labels=True, font_weight='bold', font_size=10,
+        font_color='blue', node_color='y', node_shape='o', width=2,
         edge_color="grey")
+edge_weight = nx.get_edge_attributes(G,'weight')
+nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_weight)
 plt.show()
 print('Graph is ready. Look at right!')
--- task_2.py
+++ task_2.py
@@ -3,7 +3,8 @@ import networkx as nx
 
 # Завдання 2. Напишіть функцію знаходження найкоротшого маршруту між двома
 # населеними пунктами, яка приймає як аргумент об’єкт графу і пару населених
-# пунктів, а повертає маршрут і його протяжність.
+# пунктів.
+
 
 print('--- Task 2 ---')
 
@@ -25,13 +26,11 @@ def find_shortest_path(graph, start, goal):
         path = queue.pop(0)
         node = path[-1]
 
-        # Condition to check if the
-        # current node is not visited
+        # Перевірка, чи були вже у почтоному вузлі.
         if node not in explored:
             neighbours = graph[node]
 
-            # Loop to iterate over the
-            # neighbours of the node
+            # Перевірка сусіднів вузлів.
             for neighbour in neighbours:
                 new_path = list(path)
                 new_path.append(neighbour)
@@ -57,15 +56,25 @@ if __name__ == "__main__":
     }
 
     G = nx.Graph(graph)
-    # G.add_nodes_from([('Hadiach', 'Zinkiv'), ('Lebedyn', 'Myrhorod')])
-    # G.add_edges_from([('Hadiach', 'Zinkiv'), ('Hadiach', 'Myrhorod'),
-    #                   ('Zavodske', 'Zinkiv'), ('Myrhorod', 'Zinkiv')])
-    print(nx.shortest_path_length(G, 'Hadiach', 'Zinkiv', weight='weight'))
-
-    nx.draw(G, with_labels=True, font_weight='bold', font_size=10,
-            font_color='blue', node_color='y', width=1.5, edge_color="grey")
+    G.add_edges_from([
+                      ('Zinkiv', 'Lebedyn', {'weight': 50}),
+                      ('Hadiach', 'Myrhorod', {'weight': 160}),
+                      ('Zavodske', 'Myrhorod', {'weight': 50}),
+                      ('Zinkiv', 'Hadiach', {'weight': 80}),
+                      ('Myrhorod', 'Zinkiv', {'weight': 140}),
+                      ('Myrhorod', 'Lebedyn', {'weight': 40}),
+                      ('Hadiach', 'Lebedyn', {'weight': 155}),
+                      ('Zavodske', 'Hadiach', {'weight': 95}),
+                      ('Zavodske', 'Lebedyn', {'weight': 100})
+    ])
+    # Тип візуалізації planar_layout.
+    pos = nx.planar_layout(G)
+    # Візуалізую граф.
+    nx.draw(G, pos, with_labels=True, font_weight='bold', font_size=10,
+            font_color='blue', node_color='y', width=1.5, edge_color="black")
+    edge_weight = nx.get_edge_attributes(G, 'weight')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_weight)
     plt.show()
 
     # Викликаю функцію.
     find_shortest_path(graph, 'Zinkiv', 'Zavodske')
-
